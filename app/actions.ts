5c7886a00b6e06b576824f174fbf3c4b92273960
Derick <derickchoskinson@gmail.com>
2024-01-19 03:58:42 -0600

    working app

--- app/actions.ts
+++ app/actions.ts
@@ -1,1 +1,15 @@
-'use-server'
+'use server'
+import prisma from '@/lib/prisma'
+
+export async function getUser() {
+  return await prisma.user.findUnique({
+    where: {
+      id: 'aa722ede-8cee-412d-8fcf-aba86bdfa36f'
+    },
+    include: {
+      userImages: true,
+      posts: true,
+      hashedPassword: false
+    }
+  })
+}
